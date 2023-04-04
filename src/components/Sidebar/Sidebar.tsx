577a1ca155a15813554a2ea24d6ae78b6e11b989
Denilson <dlemus@harding.edu>
2023-04-04 12:09:01 -0600

    ✨ Add search bard

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -18,6 +18,12 @@ export default function Sidebar() {
           </svg>
         </IconsContainer>
       </Header>
+      <Search>
+        <svg viewBox="0 0 788.5 1000" fill="#54656f" height="20px" width="20px">
+          <path d="M772 772c20 22.667 22 43.333 6 62l-46 46c-24 21.333-46.667 21.333-68 0L474 690c-49.333 28-101.333 42-156 42-85.333 0-159.667-31.667-223-95S0 499.333 0 414s30-158.333 90-219 132.667-91 218-91 160 31.667 224 95 96 137.667 96 223c0 58.667-15.333 112.667-46 162l190 188M94 414c0 58.667 22.667 110.667 68 156s97.333 68 156 68 109-21 151-63 63-93 63-153c0-58.667-22.667-110.333-68-155s-97.333-67-156-67-109 21-151 63-63 92.333-63 151" />
+        </svg>
+        <SearchInput placeholder="Search" />
+      </Search>
     </Container>
   );
 }
@@ -40,3 +46,22 @@ const Header = styled.div`
   padding: 10px 15px;
 `;
 
+const Search = styled.div`
+  background-color: #f0f2f5;
+  display: flex;
+  align-items: center;
+  margin: 10px 15px;
+  border-radius: 5px;
+
+  > svg {
+    margin: 0px 15px;
+  }
+`;
+
+const SearchInput = styled.input`
+  padding: 10px;
+  background-color: transparent;
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;

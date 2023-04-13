07e12a77062ea0780477e6654b70c79e3ed8445d
Denilson <dlemus@harding.edu>
2023-04-13 17:07:29 -0600

    ✨ Add chat component

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,12 +1,48 @@
+import { Avatar } from "antd";
 import styled from "styled-components";
 
 export default function Chat() {
   return (
-    <Container>Chat</Container>
-  )
+    <Container>
+      <Header>
+        <User>
+          <Avatar size="large">JD</Avatar>
+          <span>John Doe</span>
+        </User>
+        <IconsContainer></IconsContainer>
+      </Header>
+    </Container>
+  );
 }
 
 const Container = styled.div`
   flex: 1;
   background-color: #efeae2;
+`;
+
+const Header = styled.div`
+  background-color: #f0f2f5;
+  display: flex;
+  position: sticky;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 15px;
+  border-bottom: 1px solid #e9edef;
+  border-left: 1px solid #d1d7db;
+`;
+
+const User = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  font-size: 14px;
+`;
+
+const IconsContainer = styled.div`
+  display: flex;
+  gap: 25px;
+
+  > svg {
+    cursor: pointer;
+  }
 `;

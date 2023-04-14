4d9cf94adb7c22cb6ec14ca0471f0cc8e9e9dcb3
Denilson <dlemus@harding.edu>
2023-04-14 12:27:37 -0600

    ✨ Add message box icons

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -24,7 +24,20 @@ export default function Chat() {
         </IconsContainer>
       </Header>
       <MessageBox>
+        <svg viewBox="0 0 16 16" fill="#54656f" height="25px" width="25px">
+          <path
+            fillRule="evenodd"
+            d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zM5 8a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zM5.32 9.636a.75.75 0 011.038.175l.007.009c.103.118.22.222.35.31.264.178.683.37 1.285.37.602 0 1.02-.192 1.285-.371.13-.088.247-.192.35-.31l.007-.008a.75.75 0 111.222.87l-.614-.431c.614.43.614.431.613.431v.001l-.001.002-.002.003-.005.007-.014.019a1.984 1.984 0 01-.184.213c-.16.166-.338.316-.53.445-.63.418-1.37.638-2.127.629-.946 0-1.652-.308-2.126-.63a3.32 3.32 0 01-.715-.657l-.014-.02-.005-.006-.002-.003v-.002h-.001l.613-.432-.614.43a.75.75 0 01.183-1.044h.001z"
+          />
+        </svg>
+        <svg viewBox="0 0 24 24" fill="#54656f" height="25px" width="25px">
+          <path d="M20.12 11.95l-6.58 6.59a5 5 0 11-7.08-7.07l6.59-6.6a3 3 0 014.24 4.25l-6.58 6.59a1 1 0 11-1.42-1.42l6.59-6.58a1 1 0 00-1.42-1.42l-6.58 6.59a3 3 0 004.24 4.24l6.59-6.58a5 5 0 00-7.08-7.08l-6.58 6.6a7 7 0 009.9 9.9l6.59-6.6a1 1 0 00-1.42-1.4z" />
+        </svg>
         <MessageInput placeholder="Type a message" />
+        <svg viewBox="0 0 24 24" fill="#54656f" height="25px" width="25px">
+          <path d="M12 16c2.206 0 4-1.794 4-4V6c0-2.217-1.785-4.021-3.979-4.021a.933.933 0 00-.209.025A4.006 4.006 0 008 6v6c0 2.206 1.794 4 4 4z" />
+          <path d="M11 19.931V22h2v-2.069c3.939-.495 7-3.858 7-7.931h-2c0 3.309-2.691 6-6 6s-6-2.691-6-6H4c0 4.072 3.061 7.436 7 7.931z" />
+        </svg>
       </MessageBox>
     </Container>
   );
@@ -65,15 +78,20 @@ const IconsContainer = styled.div`
 
 const MessageBox = styled.div`
   display: flex;
+  align-items: center;
+  gap: 15px;
   position: absolute;
   bottom: 0;
   padding: 10px 15px;
   width: 100%;
   background-color: #f0f2f5;
+
+  > svg {
+    cursor: pointer;
+  }
 `;
 
 const MessageInput = styled.input`
- 
   font-size: 15px;
   padding: 12px 20px;
   border-radius: 8px;
